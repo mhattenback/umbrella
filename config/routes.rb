0b585af203679102bd19e58204eda7b011532c4d
Max Hattenback <max.hattenback@gmail.com>
2014-04-23 16:48:38 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ UmbrellaSite::Application.routes.draw do
   match '/contact', to: "static_pages#contact", via: 'get'
   match '/services', to: "static_pages#services", via: 'get'
   match '/gallery', to: "static_pages#gallery", via: 'get'
+  match '/materials', to: "static_pages#materials", via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
