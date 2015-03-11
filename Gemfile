96d626f50f224f2a168c0846102344ba8fd84f25
Max Hattenback <max.hattenback@gmail.com>
2015-03-11 12:15:32 -0400

    mail

--- Gemfile
+++ Gemfile
@@ -7,11 +7,13 @@ gem 'rails', '4.0.3'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 
-
+gem 'mail_form'
+gem 'simple_form'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'foreman'
 end
 
 group :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,28 @@ UmbrellaSite::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   
   config.serve_static_assets = true
+  
+  
+  #mailer
+  config.action_mailer.default_url_options = { :host => 'stormy-spire-9128.herokuapp.com' }
+
+  Rails.application.routes.default_url_options[:host] = 'yoursite.herokuapp.com'
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  
+  
+  
 end
