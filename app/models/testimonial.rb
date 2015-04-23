7d05d27991b57793382611955b874d91945a2834
Max Hattenback <max.hattenback@gmail.com>
2015-04-23 10:19:59 -0400

    testimonials

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,5 +1,19 @@
-class Testimonial < ActiveRecord::Base
+# == Schema Information
+#
+# Table name: testimonials
+#
+#  id               :integer          not null, primary key
+#  name             :string(255)
+#  address          :string(255)
+#  testimonial_body :text
+#  created_at       :datetime
+#  updated_at       :datetime
+#
 
+class Testimonial < ActiveRecord::Base
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :testimonial_body, presence: true
 
 
 end
--- spec/models/testimonial_spec.rb
+++ spec/models/testimonial_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: testimonials
+#
+#  id               :integer          not null, primary key
+#  name             :string(255)
+#  address          :string(255)
+#  testimonial_body :text
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 require 'spec_helper'
 
 describe Testimonial do
